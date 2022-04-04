1389933a7bf4dd4a742b1ab7ed29c3f457a4f1a6
Roylu7384 <roylu7384@gmail.com>
2022-04-04 11:22:18 -0400

    Debug game play logic

--- game.rb
+++ game.rb
@@ -10,7 +10,6 @@ class Game
     result = num1 + num2
     player1 = @players[0]
     player2 = @players[1]
-    score = "#{player1.name}: #{player1.lives}/3 vs #{player2.name}: #{player2.lives}/3"
 
     puts "#{player1.name}: What does #{num1} plus #{num2} equal?"
     print '>'
@@ -18,12 +17,14 @@ class Game
 
    if answer.to_i == result
      puts "#{player1.name}: YES! You are correct"
-     puts score
+     puts "#{player1.name}: #{player1.lives}/3 vs #{player2.name}: #{player2.lives}/3"
      puts "----- NEW TURN -----"
    else
      puts "#{player1.name}: Seriously? No!"
-     puts score
-     if !player1.update_live
+     over_or_not = player1.update_live
+     puts "#{player1.name}: #{player1.lives}/3 vs #{player2.name}: #{player2.lives}/3"
+     puts "----- NEW TURN -----"
+     if over_or_not
       puts "#{player2.name} wins with a score of #{player2.lives}/3"
       puts "----- GAME OVER -----"
       puts "Good bye!"
