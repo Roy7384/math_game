54144330e1ecc492512e53df68f833c73e8cc479
Roylu7384 <roylu7384@gmail.com>
2022-04-04 11:51:26 -0400

    Refactor code to separate score displaying to a method

--- game.rb
+++ game.rb
@@ -2,37 +2,44 @@ class Game
   def initialize(players)
     # players array
     @players = players
+    # identify which player is playing
+    @playing = 0
   end
 
+  def score
+    "#{@players[0].name}: #{@players[0].lives}/3 vs #{@players[1].name}: #{@players[1].lives}/3"
+  end
+    
   def play 
     num1 = rand(1..20)
     num2 = rand(1..20)
     result = num1 + num2
-    player1 = @players[0]
-    player2 = @players[1]
+    player_playing = @players[@playing]
 
-    puts "#{player1.name}: What does #{num1} plus #{num2} equal?"
+    puts "#{player_playing.name}: What does #{num1} plus #{num2} equal?"
     print '>'
     answer = $stdin.gets.chomp
 
    if answer.to_i == result
-     puts "#{player1.name}: YES! You are correct"
-     puts "#{player1.name}: #{player1.lives}/3 vs #{player2.name}: #{player2.lives}/3"
-     puts "----- NEW TURN -----"
+     puts "#{player_playing.name}: YES! You are correct"
+     puts score
    else
-     puts "#{player1.name}: Seriously? No!"
-     over_or_not = player1.update_live
-     puts "#{player1.name}: #{player1.lives}/3 vs #{player2.name}: #{player2.lives}/3"
-     if over_or_not
-      puts "#{player2.name} wins with a score of #{player2.lives}/3"
+     puts "#{player_playing.name}: Seriously? No!"
+    
+     # update player live and check if lives == 0
+     game_over = player_playing.update_live
+     puts score
+     
+     if game_over
+      puts "#{@players[1 - @playing].name} wins with a score of #{@players[1 - @playing].lives}/3"
       puts "----- GAME OVER -----"
       puts "Good bye!"
       exit(0)
+     end
     end
-    puts "----- NEW TURN -----"
-   end
 
-   @players = @players.rotate
+    puts "----- NEW TURN -----"
+    @playing = 1 - @playing
   end
 
 end
